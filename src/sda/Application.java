0267f88b11d16819fc518c948ceb5a0eb11637c2
nowszy94 <nowszy94@gmail.com>
2017-02-18 14:43:38 +0100

    Initial commit

---
+++ src/sda/Application.java
@@ -0,0 +1,10 @@
+package sda;
+
+/**
+ * Created by Szymon on 2017-02-18.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
